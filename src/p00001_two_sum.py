7b88ed185e5d7d15a20d1b73e16a05c3d6aa807d
skai273 <kinoshita116@gmail.com>
2026-04-09 17:39:40 +0900

    feat: complete 00001

--- src/p00001_two_sum.py
+++ src/p00001_two_sum.py
@@ -1,3 +1,16 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
+        num_dict: dict[int, list] = {}
+        for i, n in enumerate(nums):
+            if n in num_dict:
+                num_dict[n].append(i)
+            else:
+                num_dict[n] = [i]
+        num_keys = sorted(list(num_dict.keys()))
+        for p1 in num_keys:
+            p2 = target - p1
+            if p1 == p2 and len(num_dict[p1]) > 1:
+                return [num_dict[p1][0], num_dict[p1][1]]
+            if p2 in num_keys:
+                return [num_dict[p1][0], num_dict[p2][0]]
         return []
--- tests/test_p00001_two_sum.py
+++ tests/test_p00001_two_sum.py
@@ -9,6 +9,7 @@ from p00001_two_sum import Solution
         ([2, 7, 11, 15], 9, [0, 1]),
         ([3, 2, 4], 6, [1, 2]),
         ([3, 3], 6, [0, 1]),
+        ([3, 3, 2, 9], 12, [0, 3]),
     ],
 )
 def test_two_sum(nums, target, expected):
