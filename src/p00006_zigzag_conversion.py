baebd44e8353b499a38f72c9609fcbf38834d2d4
skai273 <kinoshita116@gmail.com>
2026-04-14 00:05:16 +0900

    feat: improve 00006

--- src/p00006_zigzag_conversion.py
+++ src/p00006_zigzag_conversion.py
@@ -2,10 +2,10 @@ class Solution:
     def convert(self, s: str, numRows: int) -> str:
         if numRows == 1:
             return s
-        parts = [""] * numRows
+        parts = [[] for _ in range(numRows)]
         for i in range(len(s)):
             m = 2 * (numRows - 1)
-            x = i // m
-            dist = min(abs(i - m * x), abs(i - m * (x + 1)))
-            parts[dist] += s[i]
-        return "".join(parts)
+            d = i % m
+            dist = min(d, m - d)
+            parts[dist].append(s[i])
+        return "".join("".join(p) for p in parts)
