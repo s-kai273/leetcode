b4fa0ef7f44e3c00c0024d4b495053654751e3f9
skai273 <kinoshita116@gmail.com>
2026-04-16 23:31:59 +0900

    feat: improve 00012

--- src/problems/p00012_integer_to_roman.py
+++ src/problems/p00012_integer_to_roman.py
@@ -1,45 +1,28 @@
 class Solution:
     def __init__(self):
-        self.symbolMap = {
-            0: {
-                1: "I",
-                4: "IV",
-                5: "V",
-                9: "IX",
-            },
-            1: {
-                1: "X",
-                4: "XL",
-                5: "L",
-                9: "XC",
-            },
-            2: {
-                1: "C",
-                4: "CD",
-                5: "D",
-                9: "CM",
-            },
-            3: {
-                1: "M",
-                4: "M" * 4,
-                5: "M" * 5,
-                9: "M" * 9,
-            },
-        }
+        self.symbolPairs = [
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I"),
+        ]
 
     def intToRoman(self, num: int) -> str:
-        answer = ""
+        answer_list = []
         val = num
-        for i in range(3, -1, -1):
-            q = val // 10**i
-            if q == 0:
-                continue
-            if q == 1 or q == 4 or q == 5 or q == 9:
-                answer += self.symbolMap[i][q]
-            elif q == 2 or q == 3:
-                answer += self.symbolMap[i][1] * q
-            elif q == 6 or q == 7 or q == 8:
-                r = q % 5
-                answer += self.symbolMap[i][5] + self.symbolMap[i][1] * r
-            val = val - q * 10**i
-        return answer
+        for pair in self.symbolPairs:
+            pnum = pair[0]
+            pstr = pair[1]
+            count = val // pnum
+            answer_list.append(pstr * count)
+            val = val - pnum * count
+        return "".join(answer_list)
