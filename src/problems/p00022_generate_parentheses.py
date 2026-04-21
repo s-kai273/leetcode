9717c583ba945963ad54efdebdf05aa7e5ea3b08
skai273 <kinoshita116@gmail.com>
2026-04-21 19:02:07 +0900

    feat: improve 00022

--- src/problems/p00022_generate_parentheses.py
+++ src/problems/p00022_generate_parentheses.py
@@ -1,27 +1,22 @@
 class Solution:
     def generateParenthesis(self, n: int) -> list[str]:
         result = []
+        target_len = 2 * n
 
-        def dfs(path, open_count, close_count):
-            pcount = len(path)
-            if pcount == 2 * n:
+        def dfs(path: list[str], open_count: int, close_count: int):
+            if len(path) == target_len:
                 result.append("".join(path))
                 return
 
             for s in ["(", ")"]:
                 if s == "(" and open_count < n:
                     path.append(s)
-                    open_count += 1
+                    dfs(path, open_count + 1, close_count)
+                    path.pop()
                 elif s == ")" and close_count < open_count:
                     path.append(s)
-                    close_count += 1
-                if len(path) > pcount:
-                    dfs(path, open_count, close_count)
-                    s = path.pop()
-                    if s == "(":
-                        open_count -= 1
-                    elif s == ")":
-                        close_count -= 1
+                    dfs(path, open_count, close_count + 1)
+                    path.pop()
 
         dfs([], 0, 0)
         return result
