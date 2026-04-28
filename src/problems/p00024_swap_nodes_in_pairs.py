ffb789699d5f3f456488c1ec4f0c5c8420830505
skai273 <kinoshita116@gmail.com>
2026-04-29 00:58:15 +0900

    feat: improve 00024

--- src/problems/p00024_swap_nodes_in_pairs.py
+++ src/problems/p00024_swap_nodes_in_pairs.py
@@ -10,22 +10,17 @@ class ListNode:
 
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        first = None
-        odd_node = None
-        while head is not None:
-            if odd_node is None:
-                odd_node = head
-            else:
-                tmp_node = head.next
-                head.next = odd_node
-                odd_node.next = tmp_node
-                print(
-                    f"## tmp_node: {tmp_node.val}, head: {head.val}, odd_node: {odd_node.val}"
-                )
-                if first is None:
-                    first = head
-                head = odd_node
-                odd_node = None
-            print(f"## head.val: {head.val}")
-            head = head.next
-        return first
+        dummy = ListNode(0, head)
+        prev = dummy
+
+        while prev.next and prev.next.next:
+            first = prev.next
+            second = first.next
+
+            first.next = second.next
+            second.next = first
+            prev.next = second
+
+            prev = first
+
+        return dummy.next
