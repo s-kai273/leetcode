319de42430e1f6192541ab12c62fdb7944b635af
skai273 <kinoshita116@gmail.com>
2026-05-07 22:16:37 +0900

    feat: implement 00217

---
+++ tests/problems/test_p00217_contais_duplicate.py
@@ -0,0 +1,16 @@
+import pytest
+
+from problems.p00217_contais_duplicate import Solution
+
+
+@pytest.mark.parametrize(
+    "nums, expected",
+    [
+        ([1, 2, 3, 1], True),
+        ([1, 2, 3, 4], False),
+        ([1, 1, 1, 3, 3, 4, 3, 2, 4, 2], True),
+    ],
+)
+def test_contains_duplicate(nums, expected):
+    solution = Solution()
+    assert solution.containsDuplicate(nums) == expected
