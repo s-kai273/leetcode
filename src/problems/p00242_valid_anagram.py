25d5d80613211c224a3d482af9809dafcc3cd0e8
skai273 <kinoshita116@gmail.com>
2026-05-07 23:29:05 +0900

    feat: improve 00242

--- src/problems/p00242_valid_anagram.py
+++ src/problems/p00242_valid_anagram.py
@@ -1,3 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        if len(s) != len(t):
+            return False
+        t_dict = dict()
+        for ch in t:
+            t_dict[ch] = t_dict.get(ch, 0) + 1
+        for ch in s:
+            count = t_dict.get(ch, 0)
+            if count == 0:
+                return False
+            t_dict[ch] -= 1
+        return True
