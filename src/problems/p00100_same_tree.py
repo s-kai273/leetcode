60ba02aed86696ba19fe212aa7eb03337841205d
skai273 <kinoshita116@gmail.com>
2026-06-09 11:50:17 +0900

    feat: improve 00100

--- src/problems/p00100_same_tree.py
+++ src/problems/p00100_same_tree.py
@@ -1,6 +1,3 @@
-from collections import deque
-
-
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -11,16 +8,10 @@ class TreeNode:
 
 class Solution:
     def isSameTree(self, p: TreeNode | None, q: TreeNode | None) -> bool:
-        queue = deque()
-        queue.append((p, q))
-        while queue:
-            p, q = queue.popleft()
-            if p is None and q is None:
-                continue
-            if p is None or q is None:
-                return False
-            if p.val != q.val:
-                return False
-            queue.append((p.left, q.left))
-            queue.append((p.right, q.right))
-        return True
+        if p is None and q is None:
+            return True
+        if p is None or q is None:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
