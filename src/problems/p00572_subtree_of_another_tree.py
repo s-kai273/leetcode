ccc734dd62040021cc89b3e02670865f5cd98bd9
skai273 <kinoshita116@gmail.com>
2026-06-10 16:11:35 +0900

    feat: improve 00572

--- src/problems/p00572_subtree_of_another_tree.py
+++ src/problems/p00572_subtree_of_another_tree.py
@@ -8,21 +8,21 @@ class TreeNode:
 
 class Solution:
     def isSubtree(self, root: TreeNode | None, subRoot: TreeNode | None) -> bool:
-        def dfs(root_node: TreeNode | None, sub_node: TreeNode | None) -> bool:
+        def isSameTree(root_node: TreeNode | None, sub_node: TreeNode | None) -> bool:
             if root_node is None and sub_node is None:
                 return True
             if root_node is None or sub_node is None:
                 return False
             if root_node.val == sub_node.val:
-                return dfs(root_node.left, sub_node.left) and dfs(
+                return isSameTree(root_node.left, sub_node.left) and isSameTree(
                     root_node.right, sub_node.right
                 )
             return False
 
-        if root is None or subRoot is None:
+        if root is None:
             return False
-        if root.val == subRoot.val:
-            result = dfs(root, subRoot)
-            if result:
-                return True
-        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+        return (
+            isSameTree(root, subRoot)
+            or self.isSubtree(root.left, subRoot)
+            or self.isSubtree(root.right, subRoot)
+        )
