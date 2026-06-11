022ea9ec157b8eaca575279ac28a7485a02dc20c
skai273 <kinoshita116@gmail.com>
2026-06-11 22:19:53 +0900

    feat: improve 00049

--- src/problems/p00049_group_anagrams.py
+++ src/problems/p00049_group_anagrams.py
@@ -1,17 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-        code_dict = dict()
+        group_dict = dict()
         for s in strs:
-            ch_dict = dict()
+            count = [0] * 26
             for ch in s:
-                ch_dict[ch] = ch_dict.get(ch, 0) + 1
-            code = ""
-            for ch in sorted(ch_dict.keys()):
-                code += f"{ch_dict[ch]}{ch}"
-            d = code_dict.get(code, [])
-            d.append(s)
-            code_dict[code] = d
-        group = list()
-        for code in code_dict.keys():
-            group.append(code_dict[code])
-        return group
+                count[ord(ch) - ord("a")] += 1
+            key = tuple(count)
+            group_dict.setdefault(key, []).append(s)
+        return list(group_dict.values())
