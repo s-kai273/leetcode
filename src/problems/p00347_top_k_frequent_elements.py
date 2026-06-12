85e2b9899802cd6c26c5a876cd57952f3ee2e2ee
skai273 <kinoshita116@gmail.com>
2026-06-12 14:46:19 +0900

    feat: improve 00347

--- src/problems/p00347_top_k_frequent_elements.py
+++ src/problems/p00347_top_k_frequent_elements.py
@@ -3,14 +3,14 @@ class Solution:
         val_count_dict = dict()
         for val in nums:
             val_count_dict[val] = val_count_dict.get(val, 0) + 1
-        count_val_dict = dict()
+        count_val_list = [[] for _ in range(len(nums) + 1)]
         for val, count in val_count_dict.items():
-            count_val_dict.setdefault(count, []).append(val)
+            count_val_list[count].append(val)
         count = 0
         top_k_val_list = list()
-        for c in sorted(count_val_dict.keys(), reverse=True):
-            top_k_val_list += count_val_dict[c]
-            count += len(count_val_dict[c])
+        for c in range(len(nums), 0, -1):
+            top_k_val_list.extend(count_val_list[c])
+            count += len(count_val_list[c])
             if count == k:
                 break
         return top_k_val_list
