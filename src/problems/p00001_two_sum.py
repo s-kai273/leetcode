63f030c5d363a53644fb12a0c8a7e767a4ec36ac
skai273 <kinoshita116@gmail.com>
2026-06-19 13:05:44 +0900

    feat: improve 00001

--- src/problems/p00001_two_sum.py
+++ src/problems/p00001_two_sum.py
@@ -1,16 +1,10 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        num_dict: dict[int, list] = {}
-        for i, n in enumerate(nums):
-            if n in num_dict:
-                num_dict[n].append(i)
-            else:
-                num_dict[n] = [i]
-        num_keys = sorted(list(num_dict.keys()))
-        for p1 in num_keys:
-            p2 = target - p1
-            if p1 == p2 and len(num_dict[p1]) > 1:
-                return [num_dict[p1][0], num_dict[p1][1]]
-            if p1 != p2 and p2 in num_dict:
-                return [num_dict[p1][0], num_dict[p2][0]]
+        nums_dict = dict()
+        for i in range(len(nums)):
+            first = nums[i]
+            second = target - first
+            if second in nums_dict:
+                return [nums_dict[second], i]
+            nums_dict[first] = i
         return []
