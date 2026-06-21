cf38ed0fa61b4b51651ec2a388b905aeec7a32f7
skai273 <kinoshita116@gmail.com>
2026-06-21 09:17:06 +0900

    feat: improve 00567

--- src/problems/p00567_permutaion_in_string.py
+++ src/problems/p00567_permutaion_in_string.py
@@ -1,26 +1,23 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        len_s1 = len(s1)
+        len_s2 = len(s2)
         s1_ch_count = dict()
         for ch in s1:
             s1_ch_count[ch] = s1_ch_count.get(ch, 0) + 1
-        s2_ch_count = dict()
-        start, end = 0, 0
-        while start < len(s2):
-            if s2[start] not in s1_ch_count:
-                start += 1
-                continue
-            end = start
-            while end < len(s2):
-                ch = s2[end]
-                if ch in s1_ch_count and s1_ch_count[ch] > s2_ch_count.setdefault(
-                    ch, 0
-                ):
-                    s2_ch_count[ch] += 1
-                    if s1_ch_count == s2_ch_count:
-                        return True
-                    end += 1
-                else:
-                    start += 1
-                    break
-            s2_ch_count = dict()
+        window_ch_count = dict()
+        for ch in s2[:len_s1]:
+            window_ch_count[ch] = window_ch_count.get(ch, 0) + 1
+
+        if s1_ch_count == window_ch_count:
+            return True
+
+        for right in range(len_s1, len_s2):
+            left = right - len_s1
+            window_ch_count[s2[left]] -= 1
+            window_ch_count[s2[right]] = window_ch_count.get(s2[right], 0) + 1
+            if window_ch_count[s2[left]] == 0:
+                del window_ch_count[s2[left]]
+            if s1_ch_count == window_ch_count:
+                return True
         return False
