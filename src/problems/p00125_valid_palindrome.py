1fd7520624f3576b4b072e7ec0b5a5dcd5d2718f
skai273 <kinoshita116@gmail.com>
2026-06-21 19:12:23 +0900

    feat: improve 00125

--- src/problems/p00125_valid_palindrome.py
+++ src/problems/p00125_valid_palindrome.py
@@ -1,13 +1,16 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = s.lower()
-        sanitized_ch_list = []
-        for ch in s:
-            if "a" <= ch <= "z" or "0" <= ch <= "9":
-                sanitized_ch_list.append(ch)
-        s = "".join(sanitized_ch_list)
-        s_len = len(s)
-        for i in range(s_len // 2):
-            if s[i] != s[s_len - 1 - i]:
+        start = 0
+        end = len(s) - 1
+        while start < end:
+            if not s[start].isalnum():
+                start += 1
+                continue
+            if not s[end].isalnum():
+                end -= 1
+                continue
+            if s[start].lower() != s[end].lower():
                 return False
+            start += 1
+            end -= 1
         return True
