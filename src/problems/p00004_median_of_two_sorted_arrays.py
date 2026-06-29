c4e823582fcd0c0e34a8d84df3368897868fe2c0
skai273 <kinoshita116@gmail.com>
2026-06-30 08:25:17 +0900

    feat: improve 00004

--- src/problems/p00004_median_of_two_sorted_arrays.py
+++ src/problems/p00004_median_of_two_sorted_arrays.py
@@ -1,7 +1,32 @@
 class Solution:
+    def findKthSmallest(self, nums1: list[int], nums2: list[int], k: int):
+        i_nums1, i_nums2 = 0, 0
+
+        while True:
+            if i_nums1 == len(nums1):
+                return nums2[i_nums2 + k - 1]
+            if i_nums2 == len(nums2):
+                return nums1[i_nums1 + k - 1]
+
+            if k == 1:
+                return min(nums1[i_nums1], nums2[i_nums2])
+            half = k // 2
+            take1 = min(half, len(nums1) - i_nums1)
+            take2 = min(half, len(nums2) - i_nums2)
+            val1 = nums1[i_nums1 + take1 - 1]
+            val2 = nums2[i_nums2 + take2 - 1]
+            if val1 <= val2:
+                i_nums1 += take1
+                k -= take1
+            else:
+                i_nums2 += take2
+                k -= take2
+
     def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
-        arr = sorted(nums1 + nums2)
-        arr_len = len(arr)
-        if arr_len % 2 == 1:
-            return arr[arr_len // 2]
-        return (arr[arr_len // 2 - 1] + arr[arr_len // 2]) / 2
+        total_len = len(nums1) + len(nums2)
+        if total_len % 2 == 1:
+            return self.findKthSmallest(nums1, nums2, total_len // 2 + 1)
+        return (
+            self.findKthSmallest(nums1, nums2, total_len // 2)
+            + self.findKthSmallest(nums1, nums2, total_len // 2 + 1)
+        ) / 2.0
