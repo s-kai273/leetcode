315cc62852b5048ad6c031ece95e546c5083fd92
skai273 <kinoshita116@gmail.com>
2026-07-03 10:09:40 +0900

    feat: improve 00002

--- src/problems/p00002_add_two_numbers.py
+++ src/problems/p00002_add_two_numbers.py
@@ -9,24 +9,32 @@ class Solution:
     def addTwoNumbers(
         self, l1: ListNode | None, l2: ListNode | None
     ) -> ListNode | None:
-        dummy_sum = ListNode(0)
         cur_l1 = l1
         cur_l2 = l2
-        cur_sum = dummy_sum
         carry = 0
+        if not cur_l1:
+            return cur_l2
+        prev_l = None
         while cur_l1 and cur_l2:
-            cur_sum.next = ListNode((cur_l1.val + cur_l2.val + carry) % 10)
-            carry = (cur_l1.val + cur_l2.val + carry) // 10
-            cur_l1 = cur_l1.next
-            cur_l2 = cur_l2.next
-            cur_sum = cur_sum.next
-        rest_l = cur_l1 if not cur_l2 else cur_l2
+            cur_l1.val += cur_l2.val + carry
+            carry = cur_l1.val // 10
+            cur_l1.val %= 10
+            prev_l = cur_l1
+            if cur_l1.next:
+                cur_l1 = cur_l1.next
+                cur_l2 = cur_l2.next
+            else:
+                cur_l1.next = cur_l2.next
+                cur_l1 = cur_l1.next
+                break
         while carry > 0:
-            l_val = rest_l.val if rest_l else 0
-            cur_sum.next = ListNode((l_val + carry) % 10)
-            carry = (l_val + carry) // 10
-            if rest_l:
-                rest_l = rest_l.next
-            cur_sum = cur_sum.next
-        cur_sum.next = rest_l
-        return dummy_sum.next
+            if cur_l1:
+                cur_l1.val += carry
+                carry = cur_l1.val // 10
+                cur_l1.val %= 10
+                prev_l = cur_l1
+                cur_l1 = cur_l1.next
+            else:
+                prev_l.next = ListNode(carry)
+                break
+        return l1
