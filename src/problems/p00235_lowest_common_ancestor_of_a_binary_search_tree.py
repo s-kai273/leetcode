f92e9083cf5313721da9f79d267153ab6c635a66
skai273 <kinoshita116@gmail.com>
2026-07-15 07:57:27 +0900

    feat: improve 00235

--- src/problems/p00235_lowest_common_ancestor_of_a_binary_search_tree.py
+++ src/problems/p00235_lowest_common_ancestor_of_a_binary_search_tree.py
@@ -7,43 +7,19 @@ class TreeNode:
 
 
 class Solution:
-    def find_target_path(self, root: TreeNode, target: int, path: list[int]) -> bool:
-        """
-        return reverse path from target to root
-        """
-        if root.val == target:
-            return True
-        if root.left is not None and self.find_target_path(root.left, target, path):
-            path.append(True)
-            return True
-        if root.right is not None and self.find_target_path(root.right, target, path):
-            path.append(False)
-            return True
-        return False
-
-    def find_common_node(
-        self, root: TreeNode, path1: list[int], path2: list[int]
-    ) -> TreeNode:
-        """
-        return last common value from both of path
-        """
-        i = 0
-        common_node = root
-        while True:
-            if i == len(path1) or i == len(path2):
-                break
-            if path1[-1 - i] == path2[-1 - i]:
-                common_node = common_node.left if path1[-1 - i] else common_node.right
-                i += 1
-            else:
-                break
-        return common_node
-
     def lowestCommonAncestor(
         self, root: TreeNode, p: TreeNode, q: TreeNode
     ) -> TreeNode:
-        p_path = list()
-        q_path = list()
-        self.find_target_path(root, p.val, p_path)
-        self.find_target_path(root, q.val, q_path)
-        return self.find_common_node(root, p_path, q_path)
+        current = root
+        while True:
+            # if p and q are categorized to same side, break
+            if p.val == current.val or q.val == current.val:
+                break
+            p_side = p.val < current.val
+            q_side = q.val < current.val
+            if p_side != q_side:
+                break
+            current = current.left if p_side else current.right
+            if current is None:
+                raise RuntimeError
+        return current
