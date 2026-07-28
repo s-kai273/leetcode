904ddd86a2e43f73340ad1624c83c695c8c026d7
skai273 <kinoshita116@gmail.com>
2026-07-28 12:29:07 +0900

    feat: improve 00025

--- src/problems/p00025_reverse_nodes_in_k_group.py
+++ src/problems/p00025_reverse_nodes_in_k_group.py
@@ -9,39 +9,22 @@ class ListNode:
 
 
 class Solution:
+    def reverseList(self, reverse_list: list[ListNode]):
+        top = reverse_list[-1].next
+        for i in range(1, len(reverse_list)):
+            target = reverse_list[i]
+            target.next = top
+            top = target
+        reverse_list[0].next = top
+
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        dummy = ListNode(0)
-        dummy.next = head
+        dummy = ListNode(0, head)
         current = dummy
-        node_list = []
-
-        def swap_nodes(nl: list[ListNode], si: int, ei: int):
-            ps_node = nl[si - 1]
-            s_node = nl[si]
-            pe_node = nl[ei - 1]
-            e_node = nl[ei]
-
-            if ei - si == 1:
-                tmp = e_node.next
-                ps_node.next = e_node
-                e_node.next = s_node
-                s_node.next = tmp
-            else:
-                s_tmp = s_node.next
-                e_tmp = e_node.next
-                ps_node.next = e_node
-                e_node.next = s_tmp
-                pe_node.next = s_node
-                s_node.next = e_tmp
-            nl[si], nl[ei] = nl[ei], nl[si]
-            if ei - si > 2:
-                swap_nodes(nl, si + 1, ei - 1)
-
-        while current is not None:
-            node_list.append(current)
+        reverse_list = list()
+        while current:
+            reverse_list.append(current)
             current = current.next
-            if len(node_list) == k + 1:
-                current = node_list[1]
-                swap_nodes(node_list, 1, k)
-                node_list = []
+            if len(reverse_list) == k + 1:
+                self.reverseList(reverse_list)
+                reverse_list = [reverse_list[1]]
         return dummy.next
